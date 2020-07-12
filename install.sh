c4d40f267bdf1fa1d42039ed83ff1352351bdb53
Rahul Vats <r1990v@gmail.com>
2020-07-12 09:17:42 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@
 git clone https://github.com/srwareham/docker-ffmpeg-compiler.git
 cd docker-ffmpeg-compiler
 docker build -t ffmpeg-compiler .
-docker run --rm -it -v $(pwd):/host ffmpeg-compiler bash -c "cp /root/bin/ffmpeg /root/bin/ffprobe /host && chown $(id -u):$(id -g) /host/ffmpeg && chown $(id -u):$(id -g) /host/ffprobe"
+docker run --rm -it -v $(pwd):/host ffmpeg-compiler bash -c "cp /root/bin/ffmpeg /root/bin/ffprobe /root/bin/ffplay /host && chown $(id -u):$(id -g) /host/ffmpeg && chown $(id -u):$(id -g) /host/ffprobe && chown $(id -u):$(id -g) /host/ffplay"
 docker rmi -f ffmpeg-compiler
